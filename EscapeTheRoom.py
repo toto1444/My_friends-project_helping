62199cde950b5f04c5c3d69d16ea1198a2c5f477
najalna2 <rkddustlr97@gmail.com>
2017-04-26 07:03:37 +0900

    EscapetheRoom
    병신같다

---
+++ EscapeTheRoom.py
@@ -0,0 +1,65 @@
+_author_ = 'kang111369748'
+
+print ('Title image')
+eval(input("Select menu: Game Start, Gallery, Achievement, Save File, Setting, Exit"))
+if input == 'Game Start':
+    print ('Background img')
+
+    SCRIPT = 0
+    ListScript = []
+    def defScript():
+        for i in range (0, 100):
+            ListScript[i] = #need to store scripts in array but I don't know how to do it
+        return defScript()
+    while SCRIPT == 0:
+
+        print (ListScript[i])
+        ans = eval(input("Enter/Click"))
+        if ans == "Y":
+            SCRIPT = 1
+        if ans == "N":
+            SCRIPT = 0
+
+    #print
+
+    if #click = puzzles:
+        for howmanyHint in range(0,3):
+            if #click == puzzle1
+                puzzle = 0
+                realAns = "STRING"
+                while puzzle ==0:
+                    print('puzzle1 img')
+                    print('script')
+                    userAns = eval(input('Write your answer'))
+                    if userAns == realAns:
+                        puzzle = 1
+                        howmanyHint + 1
+            if  # click == puzzle2
+                puzzle = 0
+                realAns = "STRING"
+                while puzzle == 0:
+                    print('puzzle2 img')
+                    print('script')
+                    userAns = eval(input('Write your answer'))
+                    if userAns == realAns:
+                        puzzle = 1
+                        howmanyHint + 1
+            if  # click == puzzle3
+                puzzle = 0
+                realAns = "STRING"
+                while puzzle == 0:
+                    print('puzzle3 img')
+                    print('script')
+                    userAns = eval(input('Write your answer'))
+                    if userAns == realAns:
+                        puzzle = 1
+                        howmanyHint + 1
+    elif #click = door:
+        print('Door img')
+        print('script')
+        realPassword = "PASSWORD"
+        userPassword = eval(input("Enter the password"))
+        if realPassword == userPassword:
+            print ("Door img")
+            print ("Door Open")
+        else
